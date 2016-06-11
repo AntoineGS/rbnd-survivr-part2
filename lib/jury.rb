25ca3bdb6319b18291fecafe29b8a382f573b3e0
AntoineGS <antoineg.simard@gmail.com>
2016-06-10 20:01:40 -0400

    run through the justy tests

--- lib/jury.rb
+++ lib/jury.rb
@@ -1,14 +1,26 @@
 class Jury
+attr_accessor :members
+attr_accessor :votes
 
   def initialize
-    @members_list=[]
+    @members = []
+    @votes = Hash.new(0)
   end
 
   def add_member(member)
-    @members_list.push(member)
+    @members.push(member)
   end
 
-  def members
-    @members_list
+  def cast_votes(finalist)
+    @members.length.times{ puts @votes[finalist.sample] +=1 }
+    @votes
+  end
+
+  def report_votes(final_votes)
+    final_votes.each{|finalist,i| puts "#{finalist}: #{i}"}
+  end
+
+  def announce_winner(final_votes)
+    final_votes.keys.first
   end
 end
---
+++ lib/tribe.rb
@@ -0,0 +1,10 @@
+class Tribe
+  attr_accessor :name
+
+  # def initialize(options=[])
+  #   #@name = options[:name]
+  #   #@members = options[:members]
+  # end
+
+
+end
