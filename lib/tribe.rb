4ca56d9ffc3f21198a1d89bb96108b392366186c
AntoineGS <antoineg.simard@gmail.com>
2016-06-10 20:50:44 -0400

    finished working through tribe_test.rb

--- lib/tribe.rb
+++ lib/tribe.rb
@@ -1,10 +1,20 @@
 class Tribe
-  attr_accessor :name
+  attr_accessor :name, :members
 
-  # def initialize(options=[])
-  #   #@name = options[:name]
-  #   #@members = options[:members]
-  # end
+  def initialize(options=[])
+    @name = options[:name]
+    @members = options[:members]
+    puts "created"
+  end
 
+  def to_s
+    @name
+  end
 
+  def tribal_council(options=[])
+    immune = options[:immune]
+    if immune then
+      @members.reject{|a| a == immune}.sample
+    end
+  end
 end
